e0004402d2b83f2bd8c63c2789813814e41ca6b4
azureleaf <azureleaf@users.noreply.github.com>
2019-05-09 01:08:06 +0900

    added function to parse XML and get bus stop GPS coordinate data. That works fine

--- process_xml.py
+++ process_xml.py
@@ -1,13 +1,20 @@
 import xml.etree.ElementTree as ET
 import os
+import sqlite3
 
 csv_path = os.getcwd() + '/bus_stops.csv'
-
+db_path = os.getcwd() + '/busstop.db'
+namespaces = {"jps": "http://www.gsi.go.jp/GIS/jpgis/standardSchemas"}
 
 # tree = ET.parse('P11-10_04-jgd.xml')
-tree = ET.parse('test.xml')
+tree = ET.parse('sample.xml')
 root = tree.getroot()
 
 with open(csv_path, mode="w") as f:
-    for stop in root.findall(".//DirectPosition.coordinate"):
-        print(stop)
+    # When you want to identify a tag which includes namespace,
+    # you must declare mapping of namespace to URL
+    for stop in root.findall(".//jps:GM_Point", namespaces):
+        coord_str = stop.find(".//DirectPosition.coordinate")
+        coord_arr = coord_str.text.split(' ')
+        lat, lon = coord_arr[0], coord_arr[1]
+        print(stop.attrib['id'], lat, lon)
