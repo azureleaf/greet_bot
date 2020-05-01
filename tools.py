8b8273c2aa9262bee9625e923455702e56089a43
akb <doge.wow.so.scare@gmail.com>
2020-05-01 12:45:19 +0900

    feat: Get weather forecast as well as current weather

--- tools.py
+++ tools.py
@@ -2,14 +2,20 @@ import urllib.request
 import json
 import sqlite3
 import os
+from datetime import datetime, timedelta
 from contextlib import closing
 from math import sin, cos, sqrt, atan2, radians
 
 # Set user variables
 db_path = os.getcwd() + '/busstop.db'
-open_weather_api_key = os.environ['OPEN_WEATHER_API_KEY']
 
+open_weather_api_key = os.environ['OPEN_WEATHER_API_KEY']
 open_weather_city_name = "Sendai"
+
+OPENWX = {
+    "KEY": os.environ['OPEN_WEATHER_API_KEY'],
+    "CITY": "Sendai"
+}
 
 # FOR TEST USE: coordinates of some sample points
 sendai_city_hall = {
@@ -22,24 +28,56 @@ fukushima_city_hall = {
 
 def get_weather():
 
-    # Retrieve via OpenWeather API, and read as JSON
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=' \
-        + open_weather_city_name \
-        + '&appid=' \
-        + open_weather_api_key
-    fetch_data = urllib.request.urlopen(url)
-    html = fetch_data.read()
-    data_dict = json.loads(html)
+    # Get current weather & 5 days forecast
+    fetch_dict = {}
+    for info_type in ["weather", "forecast"]:
+        # Retrieve from OpenWeather
+        url = 'http://api.openweathermap.org/data/2.5/' \
+            + info_type \
+            + '?q=' \
+            + OPENWX["CITY"] \
+            + '&appid=' \
+            + OPENWX["KEY"] \
+            + '&lang=ja'
 
-    # Extract necessary elements
-    weather = data_dict['weather'][0]['main']
-    temp_now = data_dict['main']['temp'] - 273.15
-    temp_max = data_dict['main']['temp_max'] - 273.15
+        fetch_raw = urllib.request.urlopen(url)  # Access the API
+        fetch_html = fetch_raw.read()  # Convert to HTML
+        fetch_dict[info_type] = json.loads(fetch_html)  # convert to JSON
+        fetch_raw.close()
 
-    fetch_data.close()
+    # Text messages to return
+    msgs = []
 
-    return "今日の仙台の天気は{}、予想最高気温は{}度です。今の気温は{}度ですね。" \
-        .format(weather, str(round(temp_max, 1)), str(round(temp_now, 1)))
+    # Current weather
+    curr_weather = fetch_dict['weather']['weather'][0]['description']
+    curr_temp = fetch_dict['weather']['main']['temp'] - 273.15
+    curr_pressure = fetch_dict['weather']['main']['pressure']
+    curr_humidity = fetch_dict['weather']['main']['humidity']
+    msgs.append(
+        (f"今の仙台の天気は{curr_weather}、気温{str(round(curr_temp, 1))}度、"
+         f"湿度{curr_humidity}％、"
+         f"気圧{curr_pressure}hPaですね。")
+    )
+
+    # Forecasted weather
+    for forecast in fetch_dict["forecast"]["list"]:
+        # Convert string UTC time to JST datetime
+        dt_forecast = datetime.strptime(
+            forecast["dt_txt"], "%Y-%m-%d %H:%M:%S") + timedelta(hours=9)
+        # I don't want 3-hour forecasts;
+        # e.g. drop 9:00 & 15:00, leave 12:00 & 18:00
+        if dt_forecast.hour % 6 != 0:
+            continue
+        msgs.append(
+            (f'{dt_forecast.hour}時の予報は'
+             f'気温が{str(round(forecast["main"]["temp"] - 273.15, 1))}度、'
+             f'天気は{forecast["weather"][0]["description"]}です。')
+        )
+        # I want forecasts [current, 6 hrs, 12 hrs] only
+        if len(msgs) == 3:
+            break
+
+    return msgs
 
 
 def find_closest_stops(lat, lon, range_km):
@@ -54,7 +92,7 @@ def find_closest_stops(lat, lon, range_km):
     try:
         with closing(sqlite3.connect(db_path)) as conn:
 
-            # Search range of the candidates bus stops
+            # Search inside the range of the candidates bus stops
             # This range has the rectangular shapes;
             # current position +/- certain distance (here, range_km)
             range_lat = distance_to_degree(
@@ -159,6 +197,7 @@ def distance_to_degree(d_km, lat, lon):
     return degrees
 
 
+# For debugging
 if __name__ == "__main__":
 
     print(get_weather())
